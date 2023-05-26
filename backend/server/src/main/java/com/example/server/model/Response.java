abac8d089e04f9bedf69063869cdbeb479d1535d
mariana <mariamariana.cagnoni@gmail.com>
2023-05-25 22:22:22 -0300

    annotation included

--- backend/server/src/main/java/com/example/server/model/Response.java
+++ backend/server/src/main/java/com/example/server/model/Response.java
@@ -1,13 +1,22 @@
 package com.example.server.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 /**
  * @author mariana
  * @Project: server
  */
+
+@Data
+@SuperBuilder
+@JsonInclude(NON_NULL)
 public class Response {
     protected LocalDateTime localDateTime;
     protected int statusCode;
