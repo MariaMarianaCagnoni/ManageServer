704ca9a83279ac0baa75651b3cb19169dbfd91f7
mariana <mariamariana.cagnoni@gmail.com>
2023-06-13 09:56:21 -0300

    delete method in controller created

--- backend/server/src/main/java/com/example/server/controller/ServerController.java
+++ backend/server/src/main/java/com/example/server/controller/ServerController.java
@@ -3,11 +3,9 @@ package com.example.server.controller;
 import com.example.server.enumeration.Status;
 import com.example.server.model.Response;
 import com.example.server.model.Server;
-import com.example.server.service.ServerService;
 import com.example.server.service.implementation.ServerServiceImplementation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +13,6 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 
 import static java.util.Map.of;
-import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
@@ -38,33 +35,32 @@ public class ServerController {
                         .data(of("servers", serverServiceImplementation.listServers(30)))
                         .message("server retrieve")
                         .status(OK)
-
                         .statusCode(200)
                         .build()
         );
     }
 
-    @GetMapping("/save")
+    @PostMapping("/save")
     public ResponseEntity<Response> saveServer(@RequestBody @Valid Server server) {
         return ResponseEntity.ok(
                 Response.builder()
                         .timeStamp(LocalDateTime.now())
-                        .data(of("servers", server))
-                        .message(server.getStatus() == Status.SERVER_UP ? "Ping success" : "Ping failed")
+                        .data(of("servers", serverServiceImplementation.createServer(server)))
+                        .message("server created")
                         .status(CREATED)
                         .statusCode(201)
                         .build()
         );
     }
 
-    @PostMapping("/ping/{ipAddress}")
-    public ResponseEntity<Response> createServer(@PathVariable("ipAddress") String ipAddress) throws IOException {
+    @GetMapping("/ping/{ipAddress}")
+    public ResponseEntity<Response> pingServer(@PathVariable("ipAddress") String ipAddress) throws IOException {
         Server server = serverServiceImplementation.ping(ipAddress);
         return ResponseEntity.ok(
                 Response.builder()
                         .timeStamp(LocalDateTime.now())
-                        .data(of("servers", serverServiceImplementation.createServer(server)))
-                        .message("server created")
+                        .data(of("server", server))
+                        .message(server.getStatus() == Status.SERVER_UP ? "Ping success" : "Ping failed")
                         .status(OK)
                         .statusCode(200)
                         .build()
@@ -77,6 +73,19 @@ public class ServerController {
                 Response.builder()
                         .timeStamp(LocalDateTime.now())
                         .data(of("servers", serverServiceImplementation.getServerById(id)))
+                        .message("server retrieve")
+                        .status(OK)
+                        .statusCode(200)
+                        .build()
+        );
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Response> deleteServer(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(of("server deleted", serverServiceImplementation.deleteServerById(id)))
                         .message("server retrieve")
                         .status(OK)
                         .statusCode(200)
